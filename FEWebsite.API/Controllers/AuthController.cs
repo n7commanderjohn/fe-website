b69ef8e4e2e9fb9d74c67061707224ba552c7e3a
n7commanderjohn <kimgears2@gmail.com>
2019-11-08 10:03:12 -0500

    App token creation process

--- FEWebsite.API/Controllers/AuthController.cs
+++ FEWebsite.API/Controllers/AuthController.cs
@@ -1,9 +1,14 @@
+using System;
+using System.Text;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
 using FEWebsite.API.Data.BaseServices;
 using System.Threading.Tasks;
 using FEWebsite.API.Models;
 using FEWebsite.API.DTOs;
-using FEWebsite.API.Data.DerivedServices;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Configuration;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace FEWebsite.API.Controllers
 {
@@ -12,10 +17,12 @@ namespace FEWebsite.API.Controllers
     public class AuthController : ControllerBase
     {
         private IAuthRepositoryService Repo { get; }
+        public IConfiguration Config { get; }
 
-        public AuthController(IAuthRepositoryService repo)
+        public AuthController(IAuthRepositoryService repo, IConfiguration config)
         {
             this.Repo = repo;
+            this.Config = config;
         }
 
         [HttpPost("register")]
@@ -35,6 +42,42 @@ namespace FEWebsite.API.Controllers
             var createdUser = await this.Repo.Register(newUser, userForRegisterDto.Password).ConfigureAwait(false);
 
             return this.StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            var userFromRepo = await Repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password).ConfigureAwait(false);
+
+            if (userFromRepo == null)
+            {
+                return this.Unauthorized();
+            }
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.Username),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(this.Config.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds,
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return this.Ok(new {
+                token = tokenHandler.WriteToken(token)
+            });
         }
     }
 }
