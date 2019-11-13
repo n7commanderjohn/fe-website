a8d70b98bf0ee2ad2f69c9ca7be5be2c57f91a3e
n7commanderjohn <kimgears2@gmail.com>
2019-11-13 09:52:47 -0500

    defined login method.

--- FEWebsite-SPA/src/app/nav/nav.component.ts
+++ FEWebsite-SPA/src/app/nav/nav.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../_services/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,13 +10,18 @@ export class NavComponent implements OnInit {
   loginCredentials: any = {};
   nums: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   login() {
-    console.log(this.loginCredentials);
+    // console.log(this.loginCredentials);
+    this.authService.login(this.loginCredentials).subscribe(next => {
+      console.log('Login successful.');
+    }, error => {
+      console.log('Failed to login.');
+    });
   }
 
 }
