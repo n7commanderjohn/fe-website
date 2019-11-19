ac3d756f338c11468cfadf4c377f8ad6f23a8dae
n7commanderjohn <kimgears2@gmail.com>
2019-11-19 14:05:57 -0500

    Added NgxBootstrap to Angular SPA

--- FEWebsite-SPA/src/app/app.module.ts
+++ FEWebsite-SPA/src/app/app.module.ts
@@ -4,11 +4,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
+import { BsDropdownModule } from 'ngx-bootstrap';
+
 import { AuthService } from './_services/auth.service';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+
 
 @NgModule({
    declarations: [
@@ -20,7 +23,8 @@ import { RegisterComponent } from './register/register.component';
    imports: [
       BrowserModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      BsDropdownModule.forRoot(),
    ],
    providers: [
       AuthService,
