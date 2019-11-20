6e8d972a1a482df31b976b102f6e151a546663b8
n7commanderjohn <kimgears2@gmail.com>
2019-11-20 15:52:28 -0500

    routing guards implemented

--- FEWebsite-SPA/src/app/routes.routing.ts
+++ FEWebsite-SPA/src/app/routes.routing.ts
@@ -6,10 +6,23 @@ import { HomeComponent } from './home/home.component';
 import { Routes, RouterModule } from '@angular/router';
 
 export const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'users', component: UserListComponent, canActivate: [AuthGuard] },
-  { path: 'messages', component: MessagesComponent },
-  { path: 'media', component: MediaComponent },
+  {
+    path: '',
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'home', component: HomeComponent },
+    ]
+  },
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'users', component: UserListComponent },
+      { path: 'messages', component: MessagesComponent },
+      { path: 'media', component: MediaComponent },
+    ]
+  },
   { path: '**', redirectTo: 'home', pathMatch: 'full' },
 ];
 
