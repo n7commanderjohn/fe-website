783915ba51b2e202c3f3e339ea44f0d7ce2441a6
n7commanderjohn <kimgears2@gmail.com>
2020-02-20 20:31:37 -0500

    hotfix to patch the registration page and allow for skipping of action filters.

--- FEWebsite.API/Helpers/LogUserActivity.cs
+++ FEWebsite.API/Helpers/LogUserActivity.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.DependencyInjection;
 
+using FEWebsite.API.Controllers;
 using FEWebsite.API.Data.BaseServices;
 
 namespace FEWebsite.API.Helpers
@@ -12,6 +15,16 @@ namespace FEWebsite.API.Helpers
     {
         public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
+            var actionsToSkip = new List<string>() {
+                nameof(UsersController) + "." + nameof(UsersController.GetGenders)
+            };
+
+            var currentAction = context.ActionDescriptor.DisplayName;
+            bool actionShouldBeSkipped = actionsToSkip.Any(action => currentAction.Contains(action));
+            if (actionShouldBeSkipped) {
+                return;
+            }
+
             var resultContext = await next().ConfigureAwait(false);
 
             var userId = int.Parse(resultContext.HttpContext.User
