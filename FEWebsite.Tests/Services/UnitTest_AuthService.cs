bf4d78123b7c2f383b715857f692f48592cccb68
n7commanderjohn <kimgears2@gmail.com>
2020-04-03 20:04:34 -0400

    username tests

--- FEWebsite.Tests/Services/UnitTest_AuthService.cs
+++ FEWebsite.Tests/Services/UnitTest_AuthService.cs
@@ -12,6 +12,8 @@ namespace FEWebsite.Tests
     [TestClass]
     public class UnitTest_AuthService
     {
+        private const string DOESNTEXIST = "doesn't exist";
+
         private AuthService AuthService { get; }
         private DataContext DataContext { get; }
 
@@ -30,16 +32,18 @@ namespace FEWebsite.Tests
         public void Test_CreateAndComparePassword()
         {
             var user = new User();
-            string[] pws = {"igud", "pp4pp", "123&)(*&", "hahhaha"};
+            string[] passwords = GetTestPasswords();
 
-            foreach (var pw in pws) {
-                this.AuthService.CreatePasswordHash(user, pw);
+            foreach (var password in passwords)
+            {
+                this.AuthService.CreatePasswordHash(user, password);
 
-                Assert.IsTrue(this.AuthService.ComparePassword(user, pw));
+                Assert.IsTrue(this.AuthService.ComparePassword(user, password));
             }
 
-            foreach (var pw in pws) {
-                this.AuthService.CreatePasswordHash(user, pw);
+            foreach (var password in passwords)
+            {
+                this.AuthService.CreatePasswordHash(user, password);
 
                 Assert.IsFalse(this.AuthService.ComparePassword(user, "shouldfail"));
             }
@@ -48,9 +52,10 @@ namespace FEWebsite.Tests
         [TestMethod]
         public void Test_CreateUserToken()
         {
+            var usernames = GetUsernames();
             var user = new User() {
                 Id = 1,
-                Username = "n7cmdrjohn"
+                Username = usernames[0]
             };
 
             var token = this.AuthService.CreateUserToken(user, "1fXr*D!iVNF!VK0Ib93@^n$7sq3wPF");
@@ -61,30 +66,70 @@ namespace FEWebsite.Tests
         [TestMethod]
         public async Task Test_EmailExists()
         {
-            string[] emails = { "iloveeirika6969@gmail.com", "kimgears2@gmail.com", "kimgears3@gmail.com" };
-            foreach (var email in emails)
+            foreach (var email in GetExistingEmails())
             {
                 Assert.IsTrue(await this.AuthService.EmailExists(email).ConfigureAwait(false));
             }
 
-            Assert.IsFalse(await this.AuthService.EmailExists("doesn't exist").ConfigureAwait(false));
+            Assert.IsFalse(await this.AuthService.EmailExists(DOESNTEXIST).ConfigureAwait(false));
         }
 
         [TestMethod]
         public async Task Test_EmailExistsForAnotherUser()
         {
-            string[] emails = { "iloveeirika6969@gmail.com", "kimgears2@gmail.com", "kimgears3@gmail.com" };
             var user = new User() {
-                Id = 9999
+                Id = 6969
             };
-            foreach (var email in emails)
+            foreach (var email in GetExistingEmails())
             {
                 user.Email = email;
                 Assert.IsTrue(await this.AuthService.EmailExistsForAnotherUser(user).ConfigureAwait(false));
             }
 
-            user.Email = "email doesn't exist";
+            user.Email = DOESNTEXIST;
             Assert.IsFalse(await this.AuthService.EmailExistsForAnotherUser(user).ConfigureAwait(false));
+        }
+
+        [TestMethod]
+        public async Task Test_UserNameExists()
+        {
+            foreach (var username in GetUsernames())
+            {
+                Assert.IsTrue(await this.AuthService.UserNameExists(username).ConfigureAwait(false));
+            }
+
+            Assert.IsFalse(await this.AuthService.UserNameExists(DOESNTEXIST).ConfigureAwait(false));
+        }
+
+        [TestMethod]
+        public async Task Test_UserNameExistsForAnotherUser()
+        {
+            var user = new User() {
+                Id = 6969
+            };
+            foreach (var username in GetUsernames())
+            {
+                user.Username = username;
+                Assert.IsTrue(await this.AuthService.UserNameExistsForAnotherUser(user).ConfigureAwait(false));
+            }
+
+            user.Username = DOESNTEXIST;
+            Assert.IsFalse(await this.AuthService.UserNameExistsForAnotherUser(user).ConfigureAwait(false));
+        }
+
+        private static string[] GetTestPasswords()
+        {
+            return new string[] { "igud", "pp4pp", "123&)(*&", "hahhaha" };
+        }
+
+        private static string[] GetExistingEmails()
+        {
+            return new string[] { "iloveeirika6969@gmail.com", "kimgears2@gmail.com", "kimgears3@gmail.com" };
+        }
+
+        private static string[] GetUsernames()
+        {
+            return new string[] { "n7cmdrjohn", "iloveeirika6969", "n7cmdrjohn3" };
         }
     }
 }
