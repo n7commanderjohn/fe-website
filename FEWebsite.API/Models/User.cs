9a2b5b14105105b9a5e64af71a17768df509d8e8
n7commanderjohn <kimgears2@gmail.com>
2020-04-28 01:30:50 -0400

    removed unused namespaces.

--- FEWebsite.API/Models/User.cs
+++ FEWebsite.API/Models/User.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
 
 using FEWebsite.API.Models.AbstractModels;
 using FEWebsite.API.Models.ManyToManyModels.ComboModels;
